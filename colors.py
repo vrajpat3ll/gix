5784a856972ce2b8c33fb2298635850e4e01d6ef
vrajpat3ll <vrajpat3l264@gmail.com>
2025-01-15 14:19:53 +0530

    auto commit atWed Jan 15 14:19:53 2025

---
+++ colors.py
@@ -0,0 +1,42 @@
+COLORS = {
+    "bold"             : 1,
+    "italic"           : 3,
+    "underline"        : 4,
+    "strikethrough"    : 9,
+    "gray"             : 30,
+    "red"              : 31,
+    "green"            : 32,
+    "yellow"           : 33,
+    "blue"             : 34,
+    "magenta"          : 35,
+    "cyan"             : 36,
+    "white"            : 37,
+    "inverted-gray"    : 40,
+    "inverted-red"     : 41,
+    "inverted-green"   : 42,
+    "inverted-yellow"  : 43,
+    "inverted-blue"    : 44,
+    "inverted-magenta" : 45,
+    "inverted-cyan"    : 46,
+    "inverted-white"   : 47,
+}
+
+logo = '''
+   _____ _ _   ____                _ 
+  / ____(_) | |  _ \ _    _ /SSSS | |     
+ | |    | | |_| |_) | |  | |     s| |___  
+ | |   _| | __|  __/| |  | |\SSS\ |  __ \ 
+ | |__| | | |_| |   | |__| |s    || |  | |
+  \_____| |\__|_|    \____/  SSSS/| |  | |'''
+
+def colorise(msg:str, color: str):
+    """add color to your string
+
+    Args:
+        msg (str): string to be colorised
+        color (str): style | options -> [`bold`, `italic`, `underline`, `strikethrough`, `gray`, `red`, `green`, `yellow`, `blue`, `magenta`, `cyan`, `white`, `inverted-gray`, `inverted-red`, `inverted-green`, `inverted-yellow`, `inverted-blue`, `inverted-magenta`, `inverted-cyan`, `inverted-white`]
+
+    Returns:
+        str: colored string
+    """
+    return f"\033[{COLORS[color]}m" + msg + "\033[0m"
---
+++ gitpush.py
@@ -0,0 +1,80 @@
+import argparse
+import os
+import subprocess as sp
+import time
+from colors import colorise, logo
+
+def main(args):
+    """add, commit and push git changes of the current repo
+
+    Args:
+        msg (str): commit message
+        interval_seconds (float): time gap in between 2 commits
+    """
+    if args.msg == None:
+        args.msg = "auto commit at" + time.asctime()
+
+    elif args.msg.lower() == "gpt":
+        while True:
+            api_key = input(colorise("Please provide OpenAI's API key: ", "yellow"))
+            print(f"{api_key = }")
+            res = input(colorise("Is this your API key?", "yellow") + f"{api_key} ? [y/n/Enter for y]")
+            if res == 'y' or res == '':
+                break
+
+    os.system("cls")
+
+    cmds = [
+        ["git", "add", "."],
+        ["git", "commit", "-m", args.msg],
+        ["git", "push"],
+    ]
+
+    q = colorise("<CTRL+C / CMD+C>", "red")
+    print(logo)
+    print(f"Press {q} to quit...")
+    
+    # cmds = [
+    #     ["git", "status"]
+    # ]
+    stop = False    
+    while not stop:
+
+        print("\033[9;1H", end='')
+        print(colorise("[TIME] ", "green"), colorise(time.asctime(), "green"), '\n')
+        for cmd in cmds:        
+            res = sp.run(cmd)
+                
+            try:
+                out = res.stdout
+                with open("logs.log", "w+") as f:
+                    f.write(out)
+                    print(out)
+                out = res.stderr
+                with open("logs.log", "w+") as f:
+                    f.write(out)
+                    print(out)
+            except Exception as e:
+                print(e)
+
+        time.sleep(args.interval)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--interval", "-i", 
+        type=float,
+        default=10.0, 
+        help="Interval with which you want to add commits")
+
+    parser.add_argument(
+        "--msg", "-m",
+        type=str,
+        required=False,
+        help="custom commit message for each push"
+    )
+
+    args = parser.parse_args()
+    print(logo)
+    main(args)
